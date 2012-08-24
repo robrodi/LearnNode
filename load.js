30fe77f65442f7c57e7b9473441e26db72a757c1
Rob Rodi <robert.rodi@gmail.com>
2012-08-24 08:53:16 -0700

    bleh

--- load.js
+++ load.js
@@ -1,22 +1,33 @@
 var http = require('http');
-var options = {
-  host: 'localhost',
-  port: 1337,
-  path: '/upload',
-  method: 'POST'
-};
-var numUsers = 100000;
-for(var i = 0; i < numUsers; i++){
-	//console.log(i);
-	var req = http.request(options, function(res) {
+var iterations = 10000;
+var concurrentUsers = 20;
+
+http.globalAgent.maxSockets  = concurrentUsers;
+
+for(var i = 0; i < iterations; i++){
+	var req = Go(i);
+	req.end();
+}
+
+console.log("Sent")
+
+function Go(i)
+{
+	var log = i%100 == 0  || i < 100;
+	if (log) var time = process.hrtime();
+
+	var req = http.get('http://localhost:1337/', function(res) {
 		if (!res.statusCode == 200){
 	  		console.log('STATUS: ' + res.statusCode);	
+	  	}
+	  	if (log)
+	  	{
+	  		console.log(i + " : " + (process.hrtime(time)[1] / 1000000));
 	  	}
 	});
 
 	req.on('error', function(e) {
 	  console.log('problem with request: ' + e.message);
 	});
-	req.end
-	req.end();
+	return req;
 }
